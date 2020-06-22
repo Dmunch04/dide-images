8bf96ac63cdc7a2d074be01b7b0b51d2b7429fc6
Daniel Munch <daniellmunch@gmail.com>
2020-06-22 20:50:05 +0200

    Added images source

---
+++ build.sh
@@ -0,0 +1,15 @@
+shopt -s globstar
+set -e
+
+for dockerfile in **/Dockerfile; do
+    tagPath=$(dirname "$dockerfile")
+    imagePath=$(dirname "$tagPath")
+    tag=$(basename "$tagPath")
+    image=$(basename "$imagePath")
+    imageName="munchii/${image}:${tag}"
+
+    (
+        cd "$tagPath"
+        docker build --no-cache --network host -t "$imageName" . || true
+    )
+done
---
+++ push.sh
@@ -0,0 +1,14 @@
+shopt -s globstar
+set -e
+
+for dockerfile in **/Dockerfile; do
+    tagPath=$(dirname "$dockerfile")
+    imagePath=$(dirname "$tagPath")
+    tag=$(basename "$tagPath")
+    image=$(basename "$imagePath")
+    imageName="munchii/${image}:${tag}"
+
+    echo
+    echo "Pushing $imageName"
+    docker push "$imageName"
+done
